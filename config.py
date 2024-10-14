3a6610bbf832d4563d339004fb971f136d7a4011
Johnatahenr <johnatanhenrique98@gmail.com>
2024-10-13 23:26:38 -0300

    jogoteca

---
+++ config.py
@@ -0,0 +1,10 @@
+SECRET_KEY = 'alura'
+
+SQLALCHEMY_DATABASE_URI = \
+    '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
+        SGBD = 'mysql+mysqlconnector',
+        usuario = 'root',
+        senha = '040515Thais.',
+        servidor = 'localhost',
+        database = 'jogoteca'
+    )
